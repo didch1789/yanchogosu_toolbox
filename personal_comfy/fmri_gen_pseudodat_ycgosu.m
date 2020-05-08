6b314e9168b3541321d3ad797cd57abe8e474748
didch1789 <didch1789@gmail.com>
2020-05-08 10:33:17 +0900

    updates

--- personal_comfy/fmri_gen_pseudodat_ycgosu.m
+++ personal_comfy/fmri_gen_pseudodat_ycgosu.m
@@ -1,5 +1,13 @@
-function out = fmri_gen_pseudodat_ycgosu(numtr, numregs)
+function out = fmri_gen_pseudodat_ycgosu(numtr, numregs, varargin)
 % pseudo random fmri_data with AR(1) random noise
+
+noisetype = 'gaussian';
+for i = 1:numel(varargin)
+    switch varargin{i}
+        case 'noisetype'
+            noisetype = varargin{i+1};
+    end
+end            
 
 out = randn(numtr, numregs);
 
@@ -7,8 +15,14 @@ for i = 1:numregs
     x = randi([0, 100], 1, 2);
     y = randi([-4 4], 1, 1);
     z = randi([1 3], 1, 1);
-    Mdl = arima('Constant', 0, 'AR', {0.5}, 'Variance', z);
-    noise = simulate(Mdl, numtr);
+    
+    if strcmp(noisetype, 'ar')
+        Mdl = arima('Constant', 0, 'AR', {0.5}, 'Variance', z);
+        noise = simulate(Mdl, numtr);
+    elseif strcmp(noisetype, 'gaussian')
+        noise = randn(numtr, 1);
+    end
+    
     xi = y*sin(linspace(x(1), x(2), numtr))' + z*noise;
     out(:, i) = xi;
 end
