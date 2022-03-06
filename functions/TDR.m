87b10521a10d2b450b4408a5bff2da5b13ed3b4d
yangcho <didch1789@gmail.com>
2022-03-06 06:19:43 +0000

    TDR fixed

--- functions/TDR.m
+++ functions/TDR.m
@@ -166,4 +166,42 @@ p_vc = reshape(p_vc, size(Y, 1), [], n_tv);
 out.beta_v_orth = beta_v_orth;
 out.p_vc = p_vc;
 
+%% sub-function
+function binned = bin_raster(rasterdat, binsize)
+% Inputs
+%   rasterdat : raw spike data in 1ms. (neurons X timeseries)
+%   binsize   : time bin size in ms (e.g. 10) 
+%
+% Outputs
+%   binned: binned (summed wihthin bin) raster 
+% e.g. 
+%   size(rasterdat) = 100 200 (100 neurons' 200 ms data)
+%   binsize = 10;
+%   size(binned)    = 100 20
+%
+%   size(rasterdat) = 100 205 (100 neurons' 200 ms data)
+%   binsize = 10
+%   size(binned)    = 100 20 (first 5 bin will contain 11 binsize)
+neuronnum   = size(rasterdat, 1); 
+timesize    = size(rasterdat, 2);
+Quotient    = fix(timesize / binsize);
+R           = mod(timesize, binsize);
+binned      = NaN(neuronnum, Quotient);
+
+l = 1;
+for bb = 1:Quotient
+    if R ~= 0
+        k = binsize + 1;
+        R = R - 1;
+    else
+        k = binsize;
+    end
+    
+    binned(:, bb) = sum(rasterdat(:, l:l+k-1), 2);
+    l = l + k;
+
+end
+
+end
+
 end
